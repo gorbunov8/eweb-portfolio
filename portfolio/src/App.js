25f53eab41c499b8166a97befa88ea01513e30d6
gorbunov8 <vladyslav.gorbunov@gmail.com>
2022-10-19 08:45:12 +0200

    Created Navbar
    Bootstrap Navbar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarComponent from "./components/NavbarComponent";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <NavbarComponent />
+  </div>;
 }
 
 export default App;
